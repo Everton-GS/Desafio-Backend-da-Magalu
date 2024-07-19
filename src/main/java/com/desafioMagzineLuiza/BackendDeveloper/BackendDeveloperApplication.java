f535483e857ecdecd1ebc0b06429f60fd5f463a3
unknown <everton.gs.br1@gmail.com>
2024-07-19 09:52:22 -0300

    first commit

---
+++ src/main/java/com/desafioMagzineLuiza/BackendDeveloper/BackendDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.desafioMagzineLuiza.BackendDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioMagzineLuiza/BackendDeveloper/BackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioMagzineLuiza.BackendDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
